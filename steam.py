bd72a1e5b0a028e8440871de1d69d20a727ce6ba
teamtensep <117057332+teamtensep@users.noreply.github.com>
2025-03-07 18:05:03 +0530

    Update steam.py

--- steam.py
+++ steam.py
@@ -12,16 +12,31 @@ model_load = tf.keras.models.load_model('saved_model')
 st.title('Plant Village Images Recognizer')
 
 # Define the class labels
-labels =  [
-    "Apple___Apple_scab", "Apple___Black_rot", "Apple___Cedar_apple_rust", "Apple___healthy",
-    "Corn___Cercospora_leaf_spot Gray_leaf_spot", "Corn___Common_rust", "Corn___healthy",
-    "Corn___Northern_Leaf_Blight", "Grape___Black_rot", "Grape___Esca_(Black_Measles)",
-    "Grape___healthy", "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)", "Potato___Early_blight",
-    "Potato___healthy", "Potato___Late_blight", "Tomato___Bacterial_spot", "Tomato___Early_blight",
-    "Tomato___healthy", "Tomato___Late_blight", "Tomato___Leaf_Mold", "Tomato___Septoria_leaf_spot",
-    "Tomato___Spider_mites Two-spotted_spider_mite", "Tomato___Target_Spot", "Tomato___Tomato_mosaic_virus",
-    "Tomato___Tomato_Yellow_Leaf_Curl_Virus"
-]
+labels =  ['Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'Corn___Cercospora_leaf_spot Gray_leaf_spot',
+ 'Corn___Common_rust',
+ 'Corn___Northern_Leaf_Blight',
+ 'Corn___healthy',
+ 'Grape___Black_rot',
+ 'Grape___Esca_(Black_Measles)',
+ 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 'Grape___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Tomato___Bacterial_spot',
+ 'Tomato___Early_blight',
+ 'Tomato___Late_blight',
+ 'Tomato___Leaf_Mold',
+ 'Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 'Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Tomato_mosaic_virus',
+ 'Tomato___healthy']
 
 
 # Get the uploaded image file
